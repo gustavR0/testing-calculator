cce4814933be293197e9b85764f5c8548626c80c
gustavR0 <gust040825@gmail.com>
2020-11-20 10:19:43 -0600

    add more method

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {addition} from '../addition/addition';
+import { addition } from '../addition/addition';
 import { substraction } from "../substraction/substraction";
 import { division } from "../division/division";
 import { multiplication } from "../multiplication/multiplication";
+import { exp } from '../exp/exp';
+import { sqr } from '../sqr/sqr';
+import { sqrt } from '../sqrt/sqrt';
+
 
 
 @Component({
@@ -24,24 +28,42 @@ export class UiComponent implements OnInit {
     let myresult = 0;
     myresult = addition(this.operator1, this.operator2);
     this.result = myresult;
-    }
-    
-    substraction() {
-      let myresult = 0;
-      myresult = substraction(this.operator1, this.operator2);
-      this.result = myresult;
-    } 
+  }
 
-    multiplication () {
-      let myresult = 0;
-      myresult = multiplication(this.operator1, this.operator2);
-      this.result = myresult;
-    }
+  substraction() {
+    let myresult = 0;
+    myresult = substraction(this.operator1, this.operator2);
+    this.result = myresult;
+  }
 
-    division() {
-      let myresult = 0;
-      myresult = division(this.operator1, this.operator2);
-      this.result = myresult;
-    } 
+  multiplication() {
+    let myresult = 0;
+    myresult = multiplication(this.operator1, this.operator2);
+    this.result = myresult;
+  }
+
+  division() {
+    let myresult = 0;
+    myresult = division(this.operator1, this.operator2);
+    this.result = myresult;
+  }
+
+  exp(){
+    let myResul = 0;
+    myResul = exp(this.operator1,this.operator2);
+    this.result = myResul;
+  }
+
+  sqr(){
+    let myResul = 0;
+    myResul = sqr(this.operator1);
+    this.result = myResul;
+  }
+
+  sqrt(){
+    let myResul = 0;
+    myResul = sqrt(this.operator1);
+    this.result = myResul;
+  }
 
 }
